46616939ae897340b29497d5708ee3c7531ddd97
NathanD992 <DEN0006@jmss.vic.edu.au>
2025-03-03 15:19:16 +1100

    Added dropdown JS and CSS

--- script.js
+++ script.js
@@ -41,3 +41,20 @@ document.addEventListener("DOMContentLoaded", function() {
         observer.observe(element);
     });
 });
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
