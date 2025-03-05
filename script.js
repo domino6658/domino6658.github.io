61969bd6db913e6796f369c3e6a6a768132083b2
domino6658 <dlpanozzo1234@gmail.com>
2025-03-06 09:11:31 +1100

    Make width of display banners flexible

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.addEventListener("DOMContentLoaded", function() {
     const observerOptions = {
         root: null, // Use the viewport as the root
         rootMargin: '0px',
-        threshold: 0.1 // Trigger when 10% of the element is visible
+        threshold: 0.3 // Trigger when 10% of the element is visible
     };
 
     const observer = new IntersectionObserver((entries, observer) => {
